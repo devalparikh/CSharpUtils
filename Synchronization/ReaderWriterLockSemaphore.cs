a490c8201c8e5da69230f494179a259fc5c8b992
Deval Parikh <devalpp@gmail.com>
2024-09-13 23:54:35 -0400

    Add comments to ReaderWriterLockSemaphore

--- Synchronization/ReaderWriterLockSemaphore.cs
+++ Synchronization/ReaderWriterLockSemaphore.cs
@@ -38,6 +38,7 @@ public class ReaderWriterLockSemaphore : IReaderWriterLock
         bool isLastReader = _readerCount == 0;
         if (isLastReader)
         {
+            // For the last reader, allow waiting writer to acquire a lock
             _writerSemaphore.Release();
         }
         _readerSemaphore.Release();
@@ -48,7 +49,7 @@ public class ReaderWriterLockSemaphore : IReaderWriterLock
         await _writerSemaphore.WaitAsync();
     }
 
-    public async void ReleaseWriterLock()
+    public void ReleaseWriterLock()
     {
         _writerSemaphore.Release();
     }
@@ -62,3 +63,18 @@ public class ReaderWriterLockSemaphore : IReaderWriterLock
 // AcquireWriterLock()
 // write logic ...
 // ReleaseWriterLock()
+
+// example:
+// 1. writer1 acquires write semaphore
+// 1. writer1 is writing
+// 2. reader1 acquires read semaphore
+// 2. reader1 waits on writer1
+// 3. reader2 waits on reader1
+// 4. writer1 releases write semaphore
+// 5. reader1 acquires write semaphore (writers are now blocked)
+// 6. reader1 releases read semaphore
+// 7. reader2 acquires read semaphore
+// 8. reader3, reader4, ... all readers are now acquiring/releasing read semaphore to update the counter
+//    all readers can read concurrently 
+// 9. last reader releases write semaphore
+// 10. now a writer can write again
